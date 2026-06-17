895d6a60340ae0ae0f7a7a8e5859908377e1578a
Nhat Linh <nguyennhatlinh2506@gmail.com>
2026-06-17 17:07:28 +0700

    Member 4: add models, retrofit, sharedpref and profile screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("androidx.cardview:cardview:1.0.0")
     implementation("com.github.bumptech.glide:glide:4.16.0")
+<<<<<<< Updated upstream
 
     testImplementation(libs.junit)
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
@@ -61,4 +62,12 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+=======
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+>>>>>>> Stashed changes
 }
---
+++ app/src/main/java/model/User.kt
@@ -0,0 +1,10 @@
+package com.example.vcar.model
+
+data class User(
+    val _id: String?,
+    val id: String?,
+    val fullName: String,
+    val email: String,
+    val phone: String?,
+    val role: String
+)
